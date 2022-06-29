679ce498ab39a84857a4037ae8d34974c807d1bf
MauricioVarga <mauricio.varga64@gmail.com>
2022-06-29 18:36:19 -0300

    Subo cambios en ejercicios de practica

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -19,18 +19,41 @@ texto_2 = str(input('Ingrese la segunda palabra:\n'))
 # Compare cual de las dos palabras es mayor (alfabéticamente)
 # Imprima en pantalla según corresponda
 
+if texto_1 > texto_2:
+    print(texto_1, 'es mayor a', texto_2)
+else:
+    print(texto_2,'es mayor a', texto_1)
+
 # Compare cual de las dos palabras tiene mayor
 # cantidad de letras
 # Imprima en pantalla según corresponda
 
+if len(texto_1) > len(texto_2):
+    print(texto_1, 'tiene mayor cantidad de letras que', texto_2)
+elif len(texto_2) > len(texto_1):
+    print(texto_2, 'tiene mayor cantidad de letras que', texto_1)
+else:
+    print(texto_1,'y',texto_2, 'tienen la misma cantidad de letras')
+
 # Verifique si la primera letra de la primera palabra
 # es mayor a la primera letra de la segunda palabra
 # Imprima en pantalla según corresponda
+
+if texto_1[0] > texto_2[0]:
+    print('La primera letra de', texto_1, 'es mayor')
+else:
+    print('La primera letra de', texto_1, 'no es mayor a la de', texto_2)
 
 copia_texto_1 = texto_1  # Copia de la variable texto_1
 
 # Verifique que copia_texto_1 es igual a texto_1
 # Imprima en pantalla según corresponda
 
+if copia_texto_1 == texto_1:
+    print('La primera palabra y su copia son iguales')
+
 # Verifique que copia_texto_1 es distinta a texto_2
 # Imprima en pantalla según corresponda
+
+if copia_texto_1 != texto_2:
+    print('La segunda palabra y la copia de la primera palabra son distintas')
